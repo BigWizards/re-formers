6a55d62457308525770491e764e76a227cd7a9d4
BigWizards <95jsjones@gmail.com>
2021-04-22 21:54:14 +0100

    Allow editing of users with error popups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
 
   def create
     #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
@@ -13,6 +17,21 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to @user
+      else
+        render 'edit'
+      end
+  end
+  
+  
 
   private
 
