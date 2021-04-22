37d851cb53f4bdbb0c47d78902159d4ae6fe62f8
BigWizards <95jsjones@gmail.com>
2021-04-22 19:58:57 +0100

    Add HTML form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 15},
-  validates :email, presence: true, uniqueness: { case_sensitive: false },
-  validates :password, presence: true, uniqueness: { case_sensitive: true }, 
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 15};
+  validates :email, presence: true, uniqueness: { case_sensitive: false };
+  validates :password, presence: true, uniqueness: { case_sensitive: true };
 end
